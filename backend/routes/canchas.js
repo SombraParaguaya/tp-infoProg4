e289fec803679c8755dffd47974d9e3903e5fd2d
Alexander Pereira <127963964+Al3X838@users.noreply.github.com>
2025-01-14 11:25:01 -0300

    Add files via upload

--- backend/routes/canchas.js
+++ backend/routes/canchas.js
@@ -2,18 +2,17 @@ const express = require('express');
 const router = express.Router();
 const odbc = require('odbc');
 
-// Ruta POST para conectar a la base de datos
-router.post('/', async (req, res) => {
-    const { username, password } = req.body;
-    try {
-        // Intentar la conexión usando ODBC
-        const connection = await odbc.connect(`DSN=infoProg4;UID=${username};PWD=${password}`);
-        res.status(200).send("Conexión exitosa");
-    } catch (error) {
-        res.status(500).send("Error de conexión");
-    }
-});
 
+// Ruta POST para conectar a la base de datos
+const getConnection = async () => {
+    try {
+        const connection = await odbc.connect(`DSN=infoProg4;UID=${process.env.DB_USER};PWD=${process.env.DB_PASSWORD}`);
+        return connection;
+    } catch (err) {
+        console.error('Error al conectar a la base de datos:', err);
+        throw new Error('Database connection error');
+    }   
+};
 
 
 const handleDbError = (err, res, action) => {
@@ -25,14 +24,46 @@ const handleDbError = (err, res, action) => {
     res.status(500).json({ success: false, error: `Error al ${action}: ${errorMessage}` });
 };
 
-router.get('/canchas', async (req, res) => {
+
+// obtener todas las canchas
+router.get('/', async (req, res) => {
     try {
-        const connection = await odbc.connect('DSN=infoProg4;UID=informix;PWD=informix');
-        const query = 'SELECT * FROM canchas';
+        const connection = await getConnection();
+        const result = await connection.query('SELECT * FROM CANCHAS ORDER BY NUMERO');
         await connection.close();
-        res.json({success: true, data: []});
-    } catch (error) {
-        handleDbError(error, res, 'obtener canchas');
+        res.json({ success: true, canchas: result });
+    } catch (err) {
+        handleDbError(err, res, 'fetching canchas');
+    }
+});
+
+// obtener una cancha por id
+router.get('/cancha/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const connection = await getConnection();
+        const result = await connection.query(`SELECT * FROM CANCHAS WHERE ID_CANCHA = ?`, [id]);
+        await connection.close();
+
+        if (result.length > 0) {
+            res.json({ success: true, cancha: result[0] });
+        } else {
+            res.json({ success: false, error: 'Cancha not found.' });
+        }
+    } catch (err) {
+        handleDbError(err, res, 'fetching marca by ID');
+    }
+});
+// agregar cancha
+router.post('/agregar', async (req, res) => {
+    const { numero, ubicacion, tipo_suelo } = req.body;
+    try {
+        const connection = await getConnection();
+        await connection.query('INSERT INTO CANCHAS (NUMERO, UBICACION, TIPO_SUELO, LUMINICA, BEBEDERO, BANOS CAMBIADOR, ESTADO) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [numero, ubicacion, tipo_suelo, luminica, bebedero, banos, cambiador, estado]);
+        await connection.close();
+        res.json({ success: true, message: 'Cancha agregada correctamente.' });
+    } catch (err) {
+        handleDbError(err, res, 'agregando cancha');
     }
 });
 
